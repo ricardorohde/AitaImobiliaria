3597c94c9d8e83a326d70cfbcf34614d273f6565
João <jpchagas@gmail.com>
2015-11-27 23:22:16 -0200

    boa evolução

---
+++ web_data.sql
@@ -0,0 +1,21 @@
+CREATE TABLE usuarios(
+user VARCHAR(30) PRIMARY KEY,
+password VARCHAR(30),
+reg_date TIMESTAMP
+);
+
+CREATE TABLE imoveis(
+id INT(3) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+dest VARCHAR(255),
+end VARCHAR,
+bairro,
+cep,
+num,
+comp,
+quartos
+garagem,
+metros,
+preco,
+
+);
+

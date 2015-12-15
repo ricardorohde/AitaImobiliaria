fe112e731bbad2a96fcdaefc33231af2f9669ce7
João <jpchagas@gmail.com>
2015-12-15 11:17:20 -0200

    good advanced

---
+++ php/nav.php
@@ -0,0 +1,44 @@
+<?php
+    echo <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.html"><img src="img/simboloaitaimoveis.png" alt=""></a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="about.html">Sobre Nós</a>
+                    </li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                            Imóveis
+                            <span class="caret"></span>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Aluguel</a></li>
+                            <li><a href="#">Aluguel por temporada</a></li>
+                            <li><a href="#">Venda</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="contact.html">Contato</a>
+                    </li>
+                    <li>
+                        <a href="dash.html">Acessar</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>;
+    ?>

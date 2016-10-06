aa3982d75f8caee867353238d0ae1b23f51d96b7
João Pedro <jpchagas@gmail.com>
2016-10-06 16:21:21 -0300

    Changes to be committed: 	modified:   footer.php

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <div id="footer" align="center">
     <div id="copyright">
       <div style="float:left; padding:3px;"><a href="#"><img src="images/footer_logo.png" width="42" height="35" alt="" border="0" /></a></div>
-      <div style="float:left; padding:14px 10px 10px 10px;"> Aita Imóveis.&copy; Todos os direitos reservados - TC Software</div>
+      <div style="float:left; padding:14px 10px 10px 10px;"> Aita Imóveis.&copy; Todos os direitos reservados - BUOY Labs</div>
     </div>
   </div> 
